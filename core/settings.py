3bd3863ccc45c2de471bbfd6ab4ecc37a9e0f582
Anwar Hosen <anwarhossene@gmail.com>
2024-08-06 15:15:02 +0600

    added active user count

--- core/settings.py
+++ core/settings.py
@@ -97,5 +97,6 @@ if DEBUG: #local
     STATICFILES_DIRS = (BASE_DIR/'static',)
 else: #production
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
